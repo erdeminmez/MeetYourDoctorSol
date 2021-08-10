252daba97071a21b35ed9c59e5d4608bdd3520f1
Erdem Inmez <inmez@shernet.sheridancollege.ca>
2021-08-10 14:14:38 -0400

    Create Patient Page structure is completed.

--- MeetYourDoctorApp/CreatePatientPage.xaml.cs
+++ MeetYourDoctorApp/CreatePatientPage.xaml.cs
@@ -13,38 +13,33 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace MeetYourDoctorApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class CreatePatientPage : Page
     {
-        public MainPage()
+        public CreatePatientPage()
         {
             this.InitializeComponent();
         }
-
-        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void OnCreateAccount(object sender, RoutedEventArgs e)
         {
-            LoginBtn.IsEnabled = true;
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
         }
 
-        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        private void OnBack(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void OnCreateDoctorAcc(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(CreateDoctorPage));
-        }
-
-        private void OnCreatePatientAcc(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(CreatePatientPage));
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
         }
     }
 }
