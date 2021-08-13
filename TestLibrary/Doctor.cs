f24ceab905c7fb78917ad8c3def52709eb19bb04
Erdem Inmez <inmez@shernet.sheridancollege.ca>
2021-08-13 16:54:13 -0400

    Doctor and Patient classes are modified.

--- TestLibrary/Doctor.cs
+++ TestLibrary/Doctor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MeetYourDoctorLibrary
@@ -18,21 +19,173 @@ namespace MeetYourDoctorLibrary
         Immunologist
     }
 
-    public class Doctor : User
+    public class Doctor
     {
-        private Branch _doctorBranch;
+        private string _username;
+        private string _password;
+        private string _fullName;
+        private string _email;
+        private string _phone;
+        private string _postalCode;
+        private string _sin;
+        private Branch _branch;
 
-        public Doctor(string username, string password, string fullName, string email, string phone, string address, Branch doctorBranch) : base(username, password, fullName, email, phone, address)
+        public Doctor(string username, string password, string fullName, string email, string phone, string postalCode, string sin, Branch branch)
         {
-            DoctorBranch = doctorBranch;
+            Username = username;
+            Password = password;
+            FullName = fullName;
+            Email = email;
+            Phone = phone;
+            PostalCode = postalCode;
+            Sin = sin;
+            Branch = branch;
         }
 
-        public Branch DoctorBranch
+        public string Username
         {
-            get => _doctorBranch;
+            get => _username;
             set
             {
-                _doctorBranch = value;
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter a username that contains at least 5 characters!");
+                }
+                else if (value.Length < 5)
+                {
+                    throw new Exception("You must enter a username that contains at least 5 characters!");
+                }
+                else
+                {
+                    _username = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter a password that contains at least 5 characters!");
+                }
+                else if (value.Length < 5)
+                {
+                    throw new Exception("You must enter a password that contains at least 5 characters!");
+                }
+                else
+                {
+                    _password = value;
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get => _fullName;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter your full name!");
+                }
+                else
+                {
+                    _fullName = value;
+                }
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter your email address!");
+                }
+                else
+                {
+                    _email = value;
+                }
+            }
+        }
+
+        public string Phone
+        {
+            get => _phone;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter a contact number that contains 10 digits!");
+                }
+                else if (value.Length != 10)
+                {
+                    throw new Exception("You must enter a contact number that contains 10 digits!");
+                }
+                else
+                {
+                    if (value.All(char.IsDigit))
+                        _phone = value;
+                    else
+                        throw new Exception("You must enter a contact number that contains 10 digits!");
+                }
+            }
+        }
+
+        public string PostalCode
+        {
+            get => _postalCode;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter a contact number that contains 6 characters!");
+                }
+                else if (value.Length != 6)
+                {
+                    throw new Exception("You must enter a contact number that contains 6 characters!");
+                }
+                else
+                {
+                    _postalCode = value;
+                }
+            }
+        }
+
+        public string Sin
+        {
+            get => _sin;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter a SIN that contains 9 digits!");
+                }
+                else if (value.Length != 9)
+                {
+                    throw new Exception("You must enter a SIN that contains 9 digits!");
+                }
+                else
+                {
+                    if (value.All(char.IsDigit))
+                        _sin = value;
+                    else
+                        throw new Exception("You must enter a SIN that contains 9 digits!");
+                }
+            }
+        }
+        
+        public Branch Branch
+        {
+            get => _branch;
+            set
+            {
+                _branch = value;
             }
         }
     }
--- TestLibrary/Patient.cs
+++ TestLibrary/Patient.cs
@@ -1,18 +1,169 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MeetYourDoctorLibrary
 {
-    public class Patient : User
+    public class Patient
     {
-        private DateTime _birthday;
+        private string _username;
+        private string _password;
+        private string _fullName;
+        private string _email;
+        private string _phone;
+        private string _postalCode;
         private string _healthCardNumber;
+        private DateTime _birthday;
 
-        public Patient(string username, string password, string fullName, string email, string phone, string address, DateTime birthday, string healthCardNumber) : base(username, password, fullName, email, phone, address)
+        public Patient(string username, string password, string fullName, string email, string phone, string postalCode, string healthCardNumber, DateTime birthday)
         {
-            Birthday = birthday;
+            Username = username;
+            Password = password;
+            FullName = fullName;
+            Email = email;
+            Phone = phone;
+            PostalCode = postalCode;
             HealthCardNumber = healthCardNumber;
+            Birthday = birthday;
+        }
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter a username that contains at least 5 characters!");
+                }
+                else if (value.Length < 5)
+                {
+                    throw new Exception("You must enter a username that contains at least 5 characters!");
+                }
+                else
+                {
+                    _username = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter a password that contains at least 5 characters!");
+                }
+                else if (value.Length < 5)
+                {
+                    throw new Exception("You must enter a password that contains at least 5 characters!");
+                }
+                else
+                {
+                    _password = value;
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get => _fullName;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter your full name!");
+                }
+                else
+                {
+                    _fullName = value;
+                }
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter your email address!");
+                }
+                else
+                {
+                    _email = value;
+                }
+            }
+        }
+
+        public string Phone
+        {
+            get => _phone;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter a contact number that contains 10 digits!");
+                }
+                else if (value.Length != 10)
+                {
+                    throw new Exception("You must enter a contact number that contains 10 digits!");
+                }
+                else
+                {
+                    if (value.All(char.IsDigit))
+                        _phone = value;
+                    else
+                        throw new Exception("You must enter a contact number that contains 10 digits!");
+                }
+            }
+        }
+
+        public string PostalCode
+        {
+            get => _postalCode;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter a contact number that contains 6 characters!");
+                }
+                else if (value.Length != 6)
+                {
+                    throw new Exception("You must enter a contact number that contains 6 characters!");
+                }
+                else
+                {
+                    _postalCode = value;
+                }
+            }
+        }
+
+        public string HealthCardNumber
+        {
+            get => _healthCardNumber;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("You must enter a health card number that contains 10 digits!");
+                }
+                else if (value.Length != 10)
+                {
+                    throw new Exception("You must enter a health card number that contains 10 digits!");
+                }
+                else
+                {
+                    if (value.All(char.IsDigit))
+                        _healthCardNumber = value;
+                    else
+                        throw new Exception("You must enter a health card number that contains 10 digits!");
+                }
+            }
         }
 
         public DateTime Birthday
@@ -26,26 +177,6 @@ namespace MeetYourDoctorLibrary
                 }
                 _birthday = value;
             }
-        }
-
-        public string HealthCardNumber
-        {
-            get => _healthCardNumber;
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                {
-                    throw new Exception("You must enter a Health Card number that contains 10 digits!");
-                }
-                else if (value.Length != 10)
-                {
-                    throw new Exception("You must enter a Health Card that contains 10 digits!");
-                }
-                else
-                {
-                    _healthCardNumber = value;
-                }
-            }
-        }
+        }        
     }
 }
